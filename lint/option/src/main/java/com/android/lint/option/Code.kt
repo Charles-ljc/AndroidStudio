bdbff4fd2f8619615cfc9fe13109f3e18c85b5d3
Charles <liujinchaods@gmail.com>
2020-06-01 14:49:38 +0800

    [Lint] Add lint options

---
+++ lint/option/src/main/java/com/android/lint/option/Code.kt
@@ -0,0 +1,10 @@
+package com.android.lint.option
+
+import java.text.SimpleDateFormat
+
+/**
+ * lint-baseline.xml会将此条信息过滤
+ */
+private val formatBaseline = SimpleDateFormat("yyyy-MM-dd")
+
+private val format = SimpleDateFormat("yyyy-MM-dd")
